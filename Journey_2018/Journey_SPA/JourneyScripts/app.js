0930e5f08b61decc1389cc7341c04332551d77d7
malloryfraiche <malloryfraiche@gmail.com>
2018-03-05 16:27:05 +0100

    bootstrap and jquery from NuGet - css style sheet - login contoller stye

---
+++ Journey_2018/Journey_SPA/JourneyScripts/app.js
@@ -0,0 +1,19 @@
+﻿// app.js includes the following:
+// 1. angularJS app information
+// 2. routing
+// 3. logging
+
+angular.module('app', []);
+
+angular.module('app').controller('login', function ($scope) {
+
+    $scope.login = "Login";
+
+});
+
+angular.module('app').controller('start', function ($scope) {
+
+    $scope.start = "This is a div in start controller";
+
+});
+
