878583a2220d31aa2282f0ef03089e5a176ac7d8
malloryfraiche <malloryfraiche@gmail.com>
2018-03-14 07:25:01 +0100

    comments on what to do back-end when buttons are clicked for login and createNewAccount views.

--- Journey_2018/Journey_SPA/JourneyScripts/createNewAccount.js
+++ Journey_2018/Journey_SPA/JourneyScripts/createNewAccount.js
@@ -4,6 +4,7 @@
 
     $scope.go = function (path) {
         $location.path(path);
+        // show a "success" div in login.html (the path you are directed to) that you have created an account.
     };
 
 });
