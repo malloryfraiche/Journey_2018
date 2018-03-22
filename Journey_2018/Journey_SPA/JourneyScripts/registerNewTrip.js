37d34256cae425ce41615ff727b4fb61f41fe13c
malloryfraiche <malloryfraiche@gmail.com>
2018-03-22 16:10:24 +0100

    registerNewTrip.html functionality

--- Journey_2018/Journey_SPA/JourneyScripts/registerNewTrip.js
+++ Journey_2018/Journey_SPA/JourneyScripts/registerNewTrip.js
@@ -1,6 +1,5 @@
 ﻿angular.module('app').controller('registerNewTrip', function ($scope, $location) {
 
-    $scope.message = "RegisterNewTrip controller (.js)";
 
     $scope.go = function (path) {
         $location.path(path);
