a0221c5284f5facad897a0279273c6f9560a63be
malloryfraiche <malloryfraiche@gmail.com>
2018-03-29 22:04:14 +0200

    md-card in the start.html

--- Journey_2018/Journey_SPA/JourneyScripts/start.js
+++ Journey_2018/Journey_SPA/JourneyScripts/start.js
@@ -1,9 +1,14 @@
 ﻿angular.module('app').controller('start', function ($scope, $location) {
-    
+
     $('#toolbar').show();
 
     $scope.go = function (path) {
         $location.path(path);
     };
 
-});
+})
+    .config(function ($mdThemingProvider) {
+
+        $mdThemingProvider.theme('dark-grey').backgroundPalette('blue-grey');
+
+    });
