53f04838b8e6c858c9fe10a1d97f13d52b44367f
malloryfraiche <malloryfraiche@gmail.com>
2018-03-31 11:09:39 +0200

    just a commit before testing the angularjs table...

--- Journey_2018/Journey_SPA/JourneyScripts/manageVehicles.js
+++ Journey_2018/Journey_SPA/JourneyScripts/manageVehicles.js
@@ -1,9 +1,21 @@
 ﻿angular.module('app').controller('manageVehicles', function ($scope, $location) {
 
+    $scope.listOfVehicles = [
+        {
+            firstKey: 'mallory',
+            secondKey: 'fraiche',
+            thirdKey: 'test'
+        },
+        {
+            firstKey: 'sara',
+            secondKey: 'fraiche',
+            thirdKey: 'test2'
+        }
+    ];
+
+
     $scope.go = function (path) {
         $location.path(path);
     };
-
-
-
+    
 });
