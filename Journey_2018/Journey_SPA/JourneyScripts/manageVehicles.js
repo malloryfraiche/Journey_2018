e6dc0f4ddaf2c6003c80598f5f75b8a414ff0192
malloryfraiche <malloryfraiche@gmail.com>
2018-03-31 19:03:06 +0200

    uppdating: manageVehicles.html

--- Journey_2018/Journey_SPA/JourneyScripts/manageVehicles.js
+++ Journey_2018/Journey_SPA/JourneyScripts/manageVehicles.js
@@ -1,5 +1,18 @@
 ﻿angular.module('app').controller('manageVehicles', function ($scope, $location) {
 
+    $scope.vehicleToBeEdited = false;
+
+    $scope.showEditDivContents = function () {
+
+        // build logic to disable/hide the appropriate button in the div that shows...
+        //if () {
+        //    $('#addVehicleButton').hide();
+        //};
+
+        $scope.vehicleToBeEdited = true;
+    };
+
+
     $scope.vehicles = [
         { registrationNumber: 'LOL123' },
         { registrationNumber: 'TES238' },
