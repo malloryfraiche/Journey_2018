e24f8a95782671a9a70796f027806d3723c5e42e
malloryfraiche <malloryfraiche@gmail.com>
2018-04-01 15:05:24 +0200

    add new vehicle dialog box and functionality.

--- Journey_2018/Journey_SPA/JourneyScripts/manageVehicles.js
+++ Journey_2018/Journey_SPA/JourneyScripts/manageVehicles.js
@@ -2,7 +2,6 @@
 
 
     $scope.showEditPromtDialog = function (ev) {
-
         $mdDialog
             .show({
                 controller: DialogController,
@@ -18,12 +17,29 @@
                 $scope.info = 'This is a simple message.';
                 $timeout(function () { $scope.info = false; }, 3000);
             });
+    };
 
+    $scope.showAddVehicleDialog = function (ev) {
+        $mdDialog
+            .show({
+                controller: DialogController,
+                templateUrl: 'Dialogs/addVehicleDialog.tmpl.html',
+                parent: angular.element(document.body),
+                targetEvent: ev,
+                clickOutsideToClose: false,
+                fullscreen: false
+            });
+            // controls the action after clicking '' in the addVehicle dialog box.
+            //.then(
+            //$scope.message = function () {
+            //    $scope.info = 'This is a simple message.';
+            //    $timeout(function () { $scope.info = false; }, 3000);
+            //});
     };
 
 
 
-
+    // data for testing.
     $scope.vehicles =
         [
             { registrationNumber: 'LOL123' },
@@ -40,7 +56,7 @@
 
 
 // to control the dialog boxes.
-function DialogController($scope, $mdDialog, $timeout) {
+function DialogController($scope, $mdDialog) {
     $scope.hide = function () {
         $mdDialog.hide();
     };
