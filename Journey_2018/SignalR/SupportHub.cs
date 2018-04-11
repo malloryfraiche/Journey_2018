39035d3077d91686e2f1b7f89e4d0cebbb0efa20
malloryfraiche <malloryfraiche@gmail.com>
2018-04-11 07:15:16 +0200

    Added scripts for SignalR in index.html.  Created SupportHub.cs file to handle chat.

---
+++ Journey_2018/SignalR/SupportHub.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Journey_2018.SignalR
+{
+    public class SupportHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            Clients.All.broadcastMessage(name, message);
+        }
+
+
+
+        public override Task OnConnected()
+        {
+            // trigger when someone connects to hub.
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            // trigger when someone disconnects from hub.
+            return base.OnDisconnected(stopCalled);
+        }
+
+        public override Task OnReconnected()
+        {
+            // triggers when someone re-connects to hub.
+            return base.OnReconnected();
+        }
+    }
+}
