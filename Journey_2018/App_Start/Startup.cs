2288f164e0b89e8c93394482924ceb8eced3d1e9
malloryfraiche <malloryfraiche@gmail.com>
2018-04-11 07:00:06 +0200

    Installed SignalR through NuGet.  Created Startup.cs class in App_Start folder.

---
+++ Journey_2018/App_Start/Startup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Journey_2018.App_Start.Startup))]
+namespace Journey_2018.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
