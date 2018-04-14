eca0f7a0805803480cd3f9183fad0af1800dabca
malloryfraiche <malloryfraiche@gmail.com>
2018-04-14 17:35:03 +0200

    manageVehicles view and functionality updates.

--- Journey_2018/Journey_SPA/JourneyScripts/manageVehicles.js
+++ Journey_2018/Journey_SPA/JourneyScripts/manageVehicles.js
@@ -12,11 +12,12 @@
                 clickOutsideToClose: false,
                 fullscreen: false
             })
-            // controls the action after clicking 'update' in the edit dialog box.
+            // controls what happens after clicking 'Update' and closing editVehicle dialog box (and coming out of the DialogController).
             .then(
             $scope.message = function () {
-                $scope.info = 'This is a simple message.';
+                $scope.info = 'You have Updated a vehicle.';
                 $timeout(function () { $scope.info = false; }, 3000);
+                ReloadPage();
             });
     };
 
@@ -46,11 +47,16 @@
     }
     ReloadPage();
 
+    $scope.deleteVehicle = function (vehicle) {
+        $http.delete(vehicleApi + '/' + vehicle.Id).then(function (data) {
+            console.log(data);
+            ReloadPage();
+        });
+    };
     
     $scope.go = function (path) {
         $location.path(path);
     };
-
 });
 
 
@@ -66,17 +72,18 @@ function DialogController($scope, $mdDialog, $http) {
 
     var vehicleApi = "http://localhost:54542/api/Vehicles";
 
+    var registrationNumber = '';
     var active = true;
     var defaultVehicle = false;
 
     $scope.vehicleModel = {
-        registrationNumber: '',
+        registrationNumber: registrationNumber,
         // have the Users GUID that is logged in be here to fill out this field in DB...
         // userId: 
-        active: true,
-        defaultVehicle: false
+        active: active,
+        defaultVehicle: defaultVehicle
     };
-
+    
     // Activate and inactivate vehicle with switch button.
     $scope.onActivationChange = function () {
         active = !active;
@@ -104,9 +111,7 @@ function DialogController($scope, $mdDialog, $http) {
 
     // ADD NEW VEHICLE - 'Save' button in the addVehicle dialog box.
     $scope.addNewVehicle = function () {
-
         console.log($scope.vehicleModel);
-
         $http({
             method: 'POST',
             url: vehicleApi,
@@ -118,29 +123,25 @@ function DialogController($scope, $mdDialog, $http) {
         }).then(function (data) {
             console.log(data);
             $mdDialog.hide();
-            
         });
     };
 
-
-
-
-
-
-    //$scope.updateVehicle = function () {
-    //    $http({
-    //        method: 'PUT',
-    //        url: vehicleApi,
-    //        data: ($scope.registrationNumberInput, $scope.activationDataInput, $scope.defaultDataInput),
-    //        headers: {
-    //            'Accept': 'application/json; charset=utf-8',
-    //            'Content-Type': 'application/json; charset=utf-8'
-    //        }
-    //    }).then(function (data) {
-    //        console.log(data);
-    //        //$scope.addVehicleInput.trigger("reset");
-    //    });
-    //};
+    // UPDATE VEHICLE - 'Update' button in the editVehicle dialog box.
+    $scope.updateVehicle = function (vehicle) {
+        console.log($scope.vehicleModel);
+        $http({
+            method: 'PUT',
+            url: vehicleApi + '/' + vehicle.Id,
+            data: $scope.vehicleModel,
+            headers: {
+                'Accept': 'application/json; charset=utf-8',
+                'Content-Type': 'application/json; charset=utf-8'
+            }
+        }).then(function (data) {
+            console.log(data);
+            $mdDialog.hide();
+        });
+    };
 
 }
 
