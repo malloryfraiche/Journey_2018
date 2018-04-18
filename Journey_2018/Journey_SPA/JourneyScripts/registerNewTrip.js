5bbe8dddd385f77d39f9635adef4683d7367a900
malloryfraiche <malloryfraiche@gmail.com>
2018-04-18 19:04:50 +0200

    Beginnings of Reverse Geocoding.

--- Journey_2018/Journey_SPA/JourneyScripts/registerNewTrip.js
+++ Journey_2018/Journey_SPA/JourneyScripts/registerNewTrip.js
@@ -6,6 +6,7 @@
     // maybe have this with $rootscope to reach it from report as well?
     //$scope.selectVehicle = "";
 
+    // to have active vehicles as drop-down option.
     $http.get(vehicleApi).then(function (response) {
         $scope.vehicles = response.data;
         // 'vehicle' here is the value - 'Active' and 'RegistrationNumber' are keys...
@@ -22,6 +23,176 @@
     var stopKmVal = '';
     var startAddressVal = '';
     var destinationAddressVal = '';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    $scope.getLocation = function () {
+
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var geocoder = new google.maps.Geocoder();
+            var latLng = new google.maps.LatLng(
+                position.coords.latitude, position.coords.longitude);
+            geocoder.geocode({
+                'latLng': latLng
+            }, function (results, status) {
+                if (status === 'OK') {
+                    if (results[0]) {
+                        //$scope.registerNewTrip.startAddress = results[0].formatted_address;
+                        startAddressVal = results[0].formatted_address;
+                        console.log(startAddressVal);
+                    } else {
+                        console.log("No results found.");
+                    }
+
+                } else {
+                    console.log("Geocoder failed due to: " + status);
+                }
+                //return startAddressVal;
+            });
+        });
+        //return false;
+    };
+
+    
+    //// This function is called from the Geocode API from the index.html file.
+    //function geoCallbackFunction() {
+
+    //    var geocoder = new google.maps.Geocoder;
+
+    //    // When user clicks the pin-icon to use their location.
+    //    $scope.getLocation() = function () {
+    //        geocodeLatLng(geocoder);
+    //    };
+
+    //}
+
+    //function geocodeLatLng(geocoder) {
+
+    //    // Function to get the Lat and Lng values from user.
+    //    //function getLatitudeLongitudeVal() {
+    //    if (navigator.geolocation) {
+    //        navigator.geolocation.getCurrentPosition(showPosition/*, showError*/);
+    //    } else {
+    //        console.log("Geolocation is not supported by this browser.");
+    //    }
+
+    //    function showPosition(position) {
+    //        console.log("Latitude: " + position.coords.latitude);
+    //        console.log("Longitude: " + position.coords.longitude);
+
+    //        var lat = position.coords.latitude;
+    //        var lng = position.coords.longitude;
+    //        var latLngStr = lat + ',' + lng;
+    //        console.log(latLngStr);
+    //        //return latLngStr;
+    //    };
+
+    //    geocoder.geocode({ 'location': latLngStr }, function (results, status) {
+    //        if (status === 'OK') {
+    //            if (results[0]) {
+    //                $scope.registerNewTrip.startAddress = results[0].formatted_address;
+    //                startAddressVal = results[0].formatted_address;
+    //                console.log(startAddressVal);
+    //            } else {
+    //                console.log("No results found.");
+    //            }
+
+    //        } else {
+    //            console.log("Geocoder failed due to: " + status);
+    //        }
+    //        //return startAddressVal;
+    //    });
+
+    //    //}
+    //    //getLatitudeLongitudeVal();
+
+    //}
+
+
+
+
+
+    //$scope.getLocation = function () {
+    //    if (navigator.geolocation) {
+    //        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    //    } else {
+    //        console.log("Geolocation is not supported by this browser.");
+    //    }
+
+    //    function showPosition(position) {
+    //        console.log("Latitude: " + position.coords.latitude);
+    //        console.log("Longitude: " + position.coords.longitude);
+
+    //        var lat = position.coords.latitude;
+    //        var lng = position.coords.longitude;
+    //        var latLngStr = lat + ',' + lng;
+    //        console.log(latLngStr);
+
+    //        var geoCoder = new google.maps.Geocoder;
+
+    //        geoCoder.geocode( { 'location': latLngStr }, function (results, status) {
+
+    //            if (status === 'OK') {
+    //                if (results[0]) {
+    //                    $scope.registerNewTrip.startAddress = results[0].formatted_address;
+    //                    startAddressVal = results[0].formatted_address;
+    //                    console.log(startAddressVal);
+    //                } else {
+    //                    console.log("No results found.");
+    //                }
+
+    //            } else {
+    //                console.log("Geocoder failed due to: " + status);
+    //            }
+
+    //        });
+    //    }
+
+    //    //return startAddressVal;
+    //};
+
+    //function showError(error) {
+    //    switch (error.code) {
+    //        case error.PERMISSION_DENIED:
+    //            console.log("User denied the request for Geolocation.");
+    //            break;
+    //        case error.POSITION_UNAVAILABLE:
+    //            console.log("Location information is unavailable.");
+    //            break;
+    //        case error.TIMEOUT:
+    //            console.log("The request to get user location timed out.");
+    //            break;
+    //        case error.UNKNOWN_ERROR:
+    //            console.log("An unknown error occurred.");
+    //            break;
+    //    }
+    //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     // the JSON data.
     $scope.registerNewTrip = {
