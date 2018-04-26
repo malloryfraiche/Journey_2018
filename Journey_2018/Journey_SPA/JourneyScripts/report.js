58dbb508b21b62b8498904bce56fbe926c94ee58
malloryfraiche <malloryfraiche@gmail.com>
2018-04-26 14:17:21 +0200

    FInished with all iTextSharp code but need to test to put everything in the same Project anyways... Currently in 2 projects but going to merge them now.

--- Journey_2018/Journey_SPA/JourneyScripts/report.js
+++ Journey_2018/Journey_SPA/JourneyScripts/report.js
@@ -1,7 +1,7 @@
 ﻿angular.module('app').controller('report', function ($scope, $location, $http, $rootScope) {
 
     var vehicleApi = "https://localhost:44399/api/Vehicles";
-
+    var generatePdfApi = "https://localhost:44399/api/reports/generate";
 
     // GET - active vehicles as drop-down options.
     $http({
@@ -26,6 +26,42 @@
     });
 
 
+
+    $scope.generatePdf = function () {
+
+        var fromDateVal = new Date();
+        var toDateVal = new Date();
+        var selectedVal = $scope.reportCreation.selectModel;
+
+        // JSON data (DownloadModel.cs).
+        $scope.reportCreation = {
+            vehicleId: selectedVal,
+            fromDate: fromDateVal,
+            toDate: toDateVal
+        };
+
+        // POST - to generate the pdf and get the url to download it...
+        $http({
+            method: 'POST',
+            url: generatePdfApi,
+            data: $scope.reportCreation,
+            headers: {
+                'Authorization': 'Bearer ' + $rootScope.token,
+                'Accept': 'application/json; charset=utf-8',
+                'Content-Type': 'application/json; charset=utf-8'
+            }
+        }).then(function (response) {
+            $scope.pdfUrl = response.data;
+            // To show the Download and view PDF button.
+            $scope.toDownloadThePdf = true;
+        }, function (error) {
+            $scope.pdfUrl = "";
+            console.log(error);
+        });
+
+
+
+    };
 
 
 
