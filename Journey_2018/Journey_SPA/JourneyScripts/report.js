d8f1a702ff69622dd55e03e0b57b00e756b7b49d
malloryfraiche <malloryfraiche@gmail.com>
2018-04-27 12:08:28 +0200

    console.log() chartClick events.

--- Journey_2018/Journey_SPA/JourneyScripts/report.js
+++ Journey_2018/Journey_SPA/JourneyScripts/report.js
@@ -40,7 +40,7 @@
             $scope.showTheChart = true;
             var trips = response.data;
             $scope.chartLabels = ["Trips between 0-20km", "Trips between 21-50km", "Trips between 51-200km"];
-            $scope.chartData = [0,0,0];
+            $scope.chartData = [0, 0, 0];
             angular.forEach(response.data, function (trip) {
                 var totalKmValue = trip.StopKilometerReading - trip.StartKilometerReading;
                 if (totalKmValue <= 20) {
@@ -51,9 +51,16 @@
                     $scope.chartData[2]++;
                 }
             }, function (error) { console.log(error); });
+            $scope.chartClick = function (event) {
+                if (event) {
+                    var index = event[0]._index;
+                    console.log(trips[index]);
+                }
+            };
         });
+
     };
-    
+
     $scope.generatePdf = function () {
         // POST - to generate the pdf and get the url to download it...
         $http({
@@ -74,7 +81,7 @@
             console.log(error);
         });
     };
-    
+
     $scope.go = function (path) {
         $location.path(path);
     };
